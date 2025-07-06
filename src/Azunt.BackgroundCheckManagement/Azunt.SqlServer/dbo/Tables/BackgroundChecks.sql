b9e34e0333432a54a7ef8a8d8ac765ec0ab17fcb
Park YongJune <redplus@redplus.net>
2025-07-06 11:19:00 +0900

    Add initial SQL script to create BackgroundChecks table for backgroundCheck module

---
+++ src/Azunt.BackgroundCheckManagement/Azunt.SqlServer/dbo/Tables/BackgroundChecks.sql
@@ -0,0 +1,20 @@
+﻿-- [0][0] 백그라운드체크: BackgroundChecks 
+CREATE TABLE [dbo].[BackgroundChecks] (
+    [ID]                BIGINT IDENTITY(1,1) PRIMARY KEY,
+    [Active]            BIT DEFAULT ((1)) NULL,
+    [BackgroundCheckID] NVARCHAR(MAX) NULL,
+    [BackgroundStatus]  NVARCHAR(MAX) NULL,
+    [CompletedAt]       DATETIMEOFFSET(7) NULL,
+    [CreatedAt]         DATETIMEOFFSET(7) NULL,
+    [CreatedBy]         NVARCHAR(70) NULL,
+    [EmployeeID]        BIGINT NULL,
+    [FileName]          NVARCHAR(MAX) NULL,
+    [InvestigationID]   BIGINT NULL,
+    [PackageID]         NVARCHAR(MAX) NULL,
+    [Provider]          NVARCHAR(MAX) NULL,
+    [ReportURL]         NVARCHAR(MAX) NULL,
+    [Score]             NVARCHAR(MAX) NULL,
+    [Status]            NVARCHAR(MAX) NULL,
+    [UpdatedAt]         DATETIMEOFFSET(7) NULL,
+    [VendorID]          BIGINT NULL
+);
